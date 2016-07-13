0999f8ad04311eb388a15b8c081b54c91b33405a
noxiddd <noxidnomis@gmail.com>
2016-07-13 03:04:00 -0500

    Update TwoChannels.java

--- main/java/com/example/jevaughnsdixon/androidgraphingui/TwoChannels.java
+++ main/java/com/example/jevaughnsdixon/androidgraphingui/TwoChannels.java
@@ -1,6 +1,7 @@
 package com.example.jevaughnsdixon.androidgraphingui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,16 +9,64 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 /**
  * Created by Jevaughn S. Dixon on 7/12/2016.
  */
 public class TwoChannels extends AppCompatActivity {
+    LineChart lineChart,lineChart2;
+    ArrayList<Entry> entries=new ArrayList<>();
+    boolean show_data_points=false;
+    float MaxYValue=40;
+    float MinYValue=-40;
+    float x=0;
+    float y=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        lineChart=(LineChart) findViewById(R.id.chart);
+        lineChart2=(LineChart)findViewById(R.id.chart2);
+        LineDataSet dataSet=new LineDataSet(graphing(),"Dataaa");
 
+        final LineData data=new LineData(dataSet);
+        data.setValueTextColor(Color.WHITE);
+        lineChart.setBackgroundColor(Color.BLACK);
+        lineChart2.setBackgroundColor(Color.BLACK);
+        /////////////////////////////////////////
+
+
+        //lineChart.getaxis
+
+        Legend legend=lineChart.getLegend();
+        legend.setTextColor(Color.WHITE);
+        legend.setForm(Legend.LegendForm.CIRCLE);
+
+        XAxis xAxis=lineChart.getXAxis();
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setAvoidFirstLastClipping(true);
+
+
+
+        YAxis yAxis=lineChart.getAxisLeft();
+        yAxis.setTextColor(Color.WHITE);
+        yAxis.setAxisMaxValue(MaxYValue);//max voltage
+        yAxis.setAxisMinValue(MinYValue);
+
+
+        lineChart.setData(data);
+        lineChart2.setData(data);
 
     }
 
@@ -47,5 +96,17 @@ public class TwoChannels extends AppCompatActivity {
 
                 return super.onOptionsItemSelected(menuItem);
         }
+    }
+
+
+    ArrayList<Entry> graphing()
+    {
+        while(x<50)
+        {
+            entries.add(new Entry(x,y));
+            x=x+0.1f;
+            y=(float)Math.sin(x)*20;
+        }
+        return entries;
     }
 }
